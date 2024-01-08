5e9f938e1a96aeb540bb22d4c713b4285c365658
Kyleigh Poultney <kyleigh.poultney@gmail.com>
2024-01-08 13:10:31 -0500

    Updating styles

--- Ravenous/src/components/Business/Business.tsx
+++ Ravenous/src/components/Business/Business.tsx
@@ -1,25 +1,44 @@
 import React from "react";
-import styles from "./Business.module.css";
+import AspectRatio from '@mui/joy/AspectRatio';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import CardOverflow from '@mui/joy/CardOverflow';
+import Divider from '@mui/joy/Divider';
+import Typography from '@mui/joy/Typography';
+
 
 export const Business = ({ business }) => {
     return (
-        <div className={styles.Business}>
-            <div className={styles.imageContainer}>
-                <img src={business.imageSrc} alt="" />
-            </div>
-            <h2>{business.name}</h2>
-            <div className={styles.BusinessInformation}>
-                <div className={styles.BusinessAddress}>
-                    <p>{business.address}</p>
-                    <p>{business.city}</p>
-                    <p>{`${business.state} ${business.zipCode}`}</p>
-                </div>
-                <div className={styles.BusinessReviews}>
-                    <h3>{business.category.toUpperCase()}</h3>
-                    <h3 className={styles.rating}>{`${business.rating} stars`}</h3>
-                    <p>{`${business.reviewCount} reviews`}</p>
-                </div>
-            </div>
+        <div>
+            <Card variant="outlined" sx={{ maxWidth: 345 }}>
+                <CardOverflow>
+                    <AspectRatio ratio="2">
+                        <img
+                            src={business.imageSrc}
+                            title={business.name}
+                            alt=""
+                        />
+                    </AspectRatio>
+                </CardOverflow>
+                <CardContent>
+                    <Typography level="title-md">
+                        {business.name}
+                    </Typography>
+                    <Typography level="body-sm">{`${business.address} ${business.city} ${business.state} ${business.zipCode}`} </Typography>
+                </CardContent>
+                <CardOverflow variant="soft" sx={{ bgcolor: 'background.level1' }}>
+                    <Divider inset="context" />
+                    <CardContent orientation="horizontal">
+                        <Typography level="body-xs" fontWeight="md" textColor="text.secondary">
+                            {business.category.toUpperCase()}
+                        </Typography>
+                        <Divider orientation="vertical" />
+                        <Typography level="body-xs" fontWeight="md" textColor="text.secondary">
+                            {`${business.rating} stars with ${business.reviewCount} reviews`}
+                        </Typography>
+                    </CardContent>
+                </CardOverflow>
+            </Card>
         </div>
     );
 };
--- Ravenous/src/components/Business/BusinessList.tsx
+++ Ravenous/src/components/Business/BusinessList.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import styles from "./Business.module.css";
 
 import { Business } from "../Business/Business";
 
 export const BusinessList = ({ businesses }) => {
     return (
-        <div className={styles.BusinessList}>
+        <div>
             {businesses.map((business) => {
                 return <Business business={business} key={business.name} />;
             })}
